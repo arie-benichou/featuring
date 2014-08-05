429017727077fcbde03b79c020e198d59c8ae943
arie.benichou <arie.benichou@gmail.com>
2014-08-05 13:08:59 +0200

    cleaning

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
+var port = 3000;
 var express = require('express');
-var http = require('http');
 var path = require('path');
-
 var app = express();
-
-app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
 app.use(express.logger('dev'));
-
-//app.use(express.bodyParser());
-//app.use(express.methodOverride());
-
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(port, function() {
+  console.log("Server started on port " + port);
 });
