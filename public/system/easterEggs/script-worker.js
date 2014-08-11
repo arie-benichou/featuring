789dd1487d92b3f418da4f6b18e777e80de28e83
arie.benichou <arie.benichou@gmail.com>
2014-08-11 04:29:47 +0200

    fixed non cleared timeout

--- public/system/easterEggs/script-worker.js
+++ public/system/easterEggs/script-worker.js
@@ -1,10 +1,12 @@
+var f= function() {
+  self.postMessage({
+    type : "toggleScreenSaver"
+  });
+};
 self.addEventListener('message', function(e) {
   self.postMessage(e.data);
   if (e.data.type == "enableScreenSaver") {
-    setTimeout(function() {
-      self.postMessage({
-        type : "toggleScreenSaver"
-      });
-    }, e.data.data);
+    clearTimeout(f);
+    setTimeout(f, e.data.data);
   }
 }, false);
